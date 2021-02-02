85777a485295ce08d2154cee8259c3c9d6f3d040
Nayeem-uddin <nayeemu02@gmail.com>
2021-02-02 22:34:03 +0600

    truthy-falsy

---
+++ truthy-falsy.js
@@ -0,0 +1,39 @@
+//There are something in javascript that can consider as truthy and falsy
+ 
+//Truthy = true,'0','something',{},[]
+// Falsy = 0,'',null,undefined,false
+
+
+// const age = 0;
+// if(age){
+//     console.log('condition is true');
+// }else{
+//     console.log('condition is false');
+// }
+
+
+// const age = false;
+// if(age){
+//     console.log('condition is true');
+// }else{
+//     console.log('condition is false');
+// }
+
+// const age = {};
+// if(age){
+//     console.log('condition is true');
+// }else{
+//     console.log('condition is false');
+// }
+
+const name = '0';
+if(name){
+    console.log('condition is true');
+}else{
+    console.log('condition is false');
+}
+
+
+
+
+
